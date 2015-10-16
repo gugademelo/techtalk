f959c2761df6a2116e4dd8c12cec8cbe60628b71
Gustavo Melo <gcmelo@sp.r7.com>
2015-10-16 10:41:46 -0300

    backbone listando json e inicio da funcao de add mais itens na collection

--- backbonejs/main.js
+++ backbonejs/main.js
@@ -13,27 +13,68 @@ var CommentsCollection = Backbone.Collection.extend({
     return json.data;
   },
   initialize: function() {
+  },
+  initialLoad: function() {
     this.fetch({
       success: function() {
 
       }
     });
+
+    return this;
   }
 });
 
 var CommentItemView = Backbone.View.extend({
   tagName: 'li',
-  template: _.template($('#comment-template').html())
+  template: _.template($('#comment-template').html()),
+
+  initialize: function(options) {
+    this.model = options.model;
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
 });
 
 var CommentsView = Backbone.View.extend({
   tagName: 'ul',
 
-  initialize: function() {
-    console.log(this.collection);
+  initialize: function(options) {
+    this.collection = options.collection;
+    this.render();
+  },
+
+  render: function() {
+    var that = this;
+    this.collection.each(function(commentModel) {
+      var commentView = new CommentItemView({ model: commentModel });
+      that.$el.append(commentView.render().$el);
+    });
+
+    $('#app-container').html(this.$el);
   }
 
 });
 
-var comments = new CommentsCollection();
-var app = new CommentsView({ collection: comments });
+var comments, app;
+comments = new CommentsCollection().fetch({
+  success: function(coll) {
+    app = new CommentsView({ collection: coll });
+  }
+});
+
+$('.new-comment-form').on('submit', function() {
+  var data = $(this).serializeArray(),
+      newComment = new Comment();
+
+  for(var i=0; i<data.length; i++) {
+    var map = {};
+    map[data[i].name] = data[i].value;
+    newComment.set(map);
+  }
+
+  debugger
+});
